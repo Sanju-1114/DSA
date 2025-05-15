74c63b5bc701ecda812633d39ae4d9364ce7db58
Sanjeev Kumar <sanju20july2002@gmail.com>
2025-05-15 17:22:18 +0530

    while loop syntax and how it works in code

--- Basics/while_loop.cpp
+++ Basics/while_loop.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 int main(){
     int count=1,n=5;
+    
+  // Syntax of while loop
+    // while (condition)
+    // {
+    //   /* code */
+    // }
+    
     while(count<=n){
         cout<<count<<" ";
         count++;
     }
+
   cout<<endl;
     return 0;
 }
