11f6e51cf54676e06398dda00bdee614a0051d2a
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-05-23 17:26:22 +0530

    maximun subarray  sum code

---
+++ Arrays&Vectors/max_subarray_sum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=7;
+    int arr[]={3,-4,5,4,-1,7,-8};
+    //int arr[]={1,2,3,4,5};
+    // for(int st=0;st<n;st++){
+    //    for(int end=st;end<n;end++){
+    //        for(int i=st;i<=end;i++){            This is used to print all subarrays
+    //            cout<<arr[i];
+    //        }
+    //        cout<<" ";
+    //    }
+    //    cout<<endl;
+    // }
+
+
+    // int maxSum=INT32_MIN;
+    // for(int st=0;st<n;st++){
+    //     int currSum=0;
+    //    for(int end=st;end<n;end++){               Maximum subarrsay sum
+    //        currSum+=arr[end];
+    //        maxSum=max(currSum,maxSum);
+    //    }
+    // }
+    // cout<<"Maximum sub array sum = "<<maxSum<<endl;
+
+    return 0;
+}
