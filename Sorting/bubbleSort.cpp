90741046b3a24aada83f1b534e293aab2986cba2
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-05-29 21:35:07 +0530

    Bubble  Sort code

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -11,6 +11,7 @@ int main() {
     }
     cout << endl;
 
+    //Bubble Sort     O(n^2)
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
