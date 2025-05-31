3ae975a5e18bbbe1c5aaa0bceb2b82d433dccac5
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-05-31 18:11:09 +0530

    Sort array with 0s 1s and 2s

---
+++ Sorting/sortAnArrayWith0s1s2s.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+                                         // LeetCode  75
+void sortColors(vector<int>& nums) {
+     // Brute Force
+     // sort(nums.begin(),nums.end());
+
+
+    // Optimized
+
+    //  int n = nums.size();
+    //  int count0=0,count1=0,count2=0;
+
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //     if(nums[i]==0){
+    //         count0++;
+    //     }
+    //     else if(nums[i]==1){
+    //         count1++;
+    //     }
+    //    else{
+    //         count2++;
+    //     }
+    //  }
+
+    //  int idx=0;
+    //  for (int i = 0; i < count0; i++)
+    //  {
+    //    nums[idx++]=0;
+    //  }
+    //  for (int i = 0; i < count1; i++)
+    //  {
+    //    nums[idx++]=1;
+    //  }
+    //  for (int i = 0; i < count2; i++)
+    //  {
+    //    nums[idx++]=2;
+    //  }
+
+
+    //Optimal (Dutch National Flag Algorithm)
+    int n = nums.size();
+    int low=0 , mid =0 , high = n-1;
+
+    while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            mid++;
+            low++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+           swap(nums[high],nums[mid]);
+           high--;
+        }
+    }
+     
+}
+
+// To print Sorted array
+void printArray(vector<int> &nums)
+{
+    for (int val : nums)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> nums = {2,0,2,1,1,0,1,2,0,0};
+    sortColors(nums);
+    printArray(nums);
+
+    return 0;
+}
