1cce3a8d0bd37f2628ae19e0fcdb7ad020470612
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-06-09 23:00:23 +0530

    C++ STL list and it's functions

---
+++ STL/list.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<vector>
+#include<list>     
+using namespace std;     
+                        
+int main(){
+    // list<int> l = {1,2,3,4,5};
+    list<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_front(0);
+    l.push_front(-1);
+    l.push_front(-2);
+
+    l.pop_back();            
+    l.pop_front();
+
+   for(int val : l){
+    cout << val << " ";
+   }
+
+    cout << endl;
+
+    return 0;
+}
+
+// list --> implemented like a Doubly Linked List --> so we can insert and delete from both side --> front and back
+   // --> random access not possible
+// LIST functions
+  // push_front()
+  // push_back()
+  // pop_front()
+  // pop_back()
+  // emplace_front()
+  // emplace_back()
+  // size
+  // erase
+  // clear
+  // begin
+  // end
+  // rbegin
+  // rend
+  // insert
+  // front
+  // back
