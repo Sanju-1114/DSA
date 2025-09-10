d1be850400723af8bb6bbb3c266c50f0d0a1c17d
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-09-10 23:43:37 +0530

    Queue Reversal

---
+++ QUEUE/queueReversal.cpp
@@ -0,0 +1,40 @@
+// Reverse a queue
+#include <iostream>
+#include <vector>
+#include<stack>
+#include<queue>
+using namespace std;
+
+void queueReversal(queue<int> &q){
+    stack<int> s;
+
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    }
+
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+}
+
+int main() {
+    queue<int>q;
+
+    for(int i=1 ; i<=5 ; i++){
+        q.push(i);
+    }
+
+    queueReversal(q);
+
+    cout << "Queue after reversal : ";
+    for(int i=0 ; i<5 ; i++){
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
