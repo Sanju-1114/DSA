917a6502320f28c0112c2d01541c276017595171
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-09-11 19:17:08 +0530

    Deque

---
+++ QUEUE/deque.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include<deque>
+using namespace std;
+
+int main() {
+    deque<int> dq;
+
+    dq.push_back(1);
+    dq.push_back(2);
+    dq.push_back(3);  // 1, 2, 3
+
+    dq.push_front(4); //4, 1, 2, 3
+    
+    dq.pop_back();  //4, 1, 2
+    
+    cout << dq.front() << " " << dq.back() << endl;
+
+    return 0;
+}
