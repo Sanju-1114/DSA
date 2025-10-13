577b8fbd217acfe52362b00a905c4f523e8d6527
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-10-13 18:35:06 +0530

    inorder predecessor and successor

---
+++ BinaryTrees/BST/inorderPredecessorAndSuccessor.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void inOrder(Node* root) {
+    if(root == NULL) return;
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+
+//Find Predecessor & Successor in BST
+
+Node* rightMostInLeftSubtree(Node* root) {
+    Node* ans;
+
+    while(root != NULL) {
+        ans = root;
+        root = root->right;
+    }
+
+    return ans;
+}
+
+Node* leftMostInRightSubtree(Node* root) {
+    Node* ans;
+
+    while(root != NULL) {
+        ans = root;
+        root = root->left;
+    }
+
+    return ans;
+}
+
+vector<int> getPredSucc(Node* root, int key) {
+    Node* curr = root;
+    Node* pred = NULL;
+    Node* succ = NULL;
+
+    while(curr != NULL) {
+        if(key < curr->data) {
+            succ = curr;
+            curr = curr->left;
+        } else if(key > curr->data){
+            pred = curr;
+            curr = curr->right;
+        } else {
+
+            if(curr->left != NULL) {
+                // inorder predecessor
+                pred = rightMostInLeftSubtree(curr->left);
+            }
+            
+            if(curr->right != NULL) {
+                // inorder successor
+                succ = leftMostInRightSubtree(curr->right);
+            }
+
+            break;
+        }
+        
+    }
+    
+    return {pred->data, succ->data};
+}
+
+int main() {
+    
+    Node* root = new Node(6);
+    root->left = new Node(4);
+    root->right = new Node(8);
+    root->left->left = new Node(1);
+    root->left->right = new Node(5);
+    root->right->left = new Node(7);
+    root->right->right = new Node(9);
+
+    int key = 7;
+    vector<int> ans = getPredSucc(root, key);
+    cout << "predecessor: " << ans[0] << endl;
+    cout << "successor: " << ans[1] << endl;
+
+    return 0;
+}
