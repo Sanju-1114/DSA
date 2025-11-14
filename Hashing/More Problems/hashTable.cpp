49c09b73eb96b97b48e5704bfed588131c284836
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-11-14 17:30:59 +0530

    hashTable

---
+++ Hashing/More Problems/hashTable.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+class Node {
+public:    
+    string key;
+    int val;
+    Node* next;
+
+    Node(string key, int val) {
+        this->key = key;
+        this->val = val;
+        next = NULL;
+    }
+
+    ~Node() {
+        if(next != NULL) {
+            delete next;
+        }
+    }
+};
+
+class HashTable {
+    int totalSize;
+    int currsize; // rehashing
+    Node** table;
+
+    int hashFunction(string key) {
+        int idx = 0;
+
+        for(int i=0; i<key.size(); i++) {
+            idx = idx + ((key[i] * key[i])) % totalSize;
+        }
+
+        return idx%totalSize;
+    }
+
+    void rehash() {   //O(n)
+        Node** oldTable = table;
+        int oldSize = totalSize;
+
+        totalSize = 2*totalSize;
+        currsize = 0;
+        table = new Node*[totalSize];
+        
+        for(int i=0; i<totalSize; i++) {
+            table[i] = NULL;
+        }
+        
+        //copy old values
+        for(int i=0; i<oldSize; i++) {
+            Node* temp = oldTable[i];
+
+            while(temp != NULL) {
+                insert(temp->key, temp->val);
+                temp = temp->next;
+            }
+
+            if(oldTable[i] != NULL) {
+                delete oldTable[i];
+            }
+        }
+
+        delete[] oldTable;
+    }
+
+public:
+    HashTable(int size=5) {
+        totalSize = size;
+        currsize = 0;
+
+        table = new Node*[totalSize];
+
+        for(int i=0; i<totalSize; i++) {
+            table[i] = NULL;
+        }
+    }
+
+    void insert(string key, int val) { // O(1) avg   
+        int idx = hashFunction(key); 
+
+        Node* newNode = new Node(key, val);
+
+        newNode->next = table[idx];
+        table[idx] = newNode;
+
+        currsize++;
+
+        double  lambda = currsize/(double)totalSize;
+
+        if(lambda > 1) {
+            rehash();    // O(n) - worst
+        }
+    }
+
+    bool exists(string key) {
+        int idx = hashFunction(key);
+        
+        Node* temp = table[idx];
+        while(temp != NULL) {
+            if(temp->key == key) { //FOUND
+                return true;
+            }
+            temp = temp->next;
+        }
+
+        return false;
+    }
+
+    int search(string key) {
+        int idx = hashFunction(key);
+        
+        Node* temp = table[idx];
+        while(temp != NULL) {
+            if(temp->key == key) { //FOUND
+                return temp->val;
+            }
+            temp = temp->next;
+        }
+
+        return -1;
+    }
+
+    void remove(string key) {   
+        int idx = hashFunction(key);
+
+        Node* temp = table[idx];
+        Node* prev = temp;
+
+        while(temp != NULL) {  
+            if(temp->key == key) { //erase   // (table[idx] = temp->next) delete head node
+                prev == temp ? table[idx] = temp->next : prev->next = temp->next;
+                break;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+    }
+
+    void print() {
+        for(int i=0; i<totalSize; i++) {
+            cout << "idx : " << i << "->";
+            Node* temp = table[i];
+
+            while(temp != NULL) {
+                cout << "(" << temp->key << "," << temp->val << ") -> ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+    }
+
+};
+
+
+int main() {
+    HashTable ht;
+
+    ht.insert("India", 150);
+    ht.insert("China", 150);
+    ht.insert("US", 50);
+    ht.insert("Nepal", 10);
+    ht.insert("UK", 20);
+
+    ht.remove("China");
+
+    ht.print();
+
+    return 0;
+}
