7edf35261c7ec44937687d60aa95da626601f3a3
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-11-20 11:00:51 +0530

    Starts with problem

---
+++ TRIE/startsWithProblem.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <unordered_map>
+using namespace std;
+
+class Node {
+public:
+    unordered_map <char, Node*> children;
+    bool endOfWord; // T or F
+    Node() {
+        endOfWord = false;
+    }
+};
+
+class Trie {
+    Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string key) { //O(L)
+        Node* temp = root;
+
+        for(int i=0; i<key.size(); i++) {
+            if (temp->children.count(key[i]) == 0 ) {
+                temp->children[key[i]] =  new Node();  // insert
+            }
+            temp = temp->children[key[i]];
+        }
+        temp->endOfWord = true;
+    }
+
+    bool search(string key) {    //O(L)
+        Node* temp = root;
+
+        for(int i=0; i<key.size(); i++) {
+            if (temp->children.count(key[i])) {
+                temp = temp->children[key[i]];
+            } else {
+                return false;
+            }
+        }
+        return temp->endOfWord;
+    }
+
+    bool startsWith(string prefix) { // O(L)
+        Node* temp = root;
+
+        for(int i=0; i<prefix.size(); i++) {
+            if (temp->children.count(prefix[i])) {
+                temp = temp->children[prefix[i]];
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+int main() {
+    vector<string> words = {"apple", "app", "mango", "man", "woman"};
+    string prefix = "app";     // Output : 1
+    // string prefix = "moon"; // Output : 0
+
+    Trie trie;
+    for(int i=0; i<words.size(); i++) {
+        trie.insert(words[i]);
+    }
+
+    cout << trie.startsWith(prefix) << endl;
+
+    return 0;
+}
